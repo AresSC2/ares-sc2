13f3bda3b7922e00d2e620fa02e0449230ea0015
august-k <55416112+august-k@users.noreply.github.com>
2023-01-30 10:32:56 -0500

    feat: add sc2 and mapanalyzer

---
+++ MapAnalyzer/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from .MapData import MapData
+from .Polygon import Polygon
+from .Region import Region
+from .constructs import ChokeArea, MDRamp, VisionBlockerArea
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution("sc2mapanalyzer")
+except DistributionNotFound:
+    __version__ = "dev"
--- src/ares/main.py
+++ src/ares/main.py
@@ -1,2 +1,29 @@
+from sc2 import maps
+from sc2.bot_ai import BotAI
+from sc2.data import Difficulty, Race
+from sc2.main import run_game
+from sc2.player import Bot, Computer
+
+from MapAnalyzer import MapData
+
+
+class WorkerRushBot(BotAI):
+    async def on_step(self, iteration):
+        if iteration == 0:
+            data = MapData(self)
+            print(data.regions)
+            for worker in self.workers:
+                worker.attack(self.enemy_start_locations[0])
+
+
+def main():
+    run_game(
+        maps.get("2000AtmospheresAIE"),
+        [Bot(Race.Zerg, WorkerRushBot()), Computer(Race.Protoss, Difficulty.Medium)],
+        realtime=False,
+        # save_replay_as="WorkerRush.SC2Replay",
+    )
+
+
 if __name__ == "__main__":
-    print("Hello World")
+    main()
