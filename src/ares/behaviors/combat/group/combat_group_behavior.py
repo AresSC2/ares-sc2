e4c248890eda966f092a77b326117ff64a8f1788
Tom Kerr <tomkerrdev@gmail.com>
2024-01-04 12:39:31 +0000

    feat: keep group safe behavior

---
+++ src/ares/behaviors/combat/group/combat_group_behavior.py
@@ -0,0 +1,76 @@
+from typing import TYPE_CHECKING, Protocol, Union
+
+from sc2.ids.ability_id import AbilityId
+from sc2.position import Point2
+from sc2.unit import Unit
+
+from ares.behaviors.behavior import Behavior
+from ares.cython_extensions.geometry import cy_distance_to
+from ares.managers.manager_mediator import ManagerMediator
+
+if TYPE_CHECKING:
+    from ares import AresBot
+
+
+class CombatGroupBehavior(Behavior, Protocol):
+    """Interface that all group combat behaviors should adhere to."""
+
+    def execute(self, ai: "AresBot", config: dict, mediator: ManagerMediator) -> bool:
+        """Execute the implemented behavior.
+
+        Parameters
+        ----------
+        ai :
+            Bot object that will be running the game.
+        config :
+            Dictionary with the data from the configuration file.
+        mediator :
+            ManagerMediator used for getting information from other managers.
+
+        Returns
+        ----------
+        bool :
+            CombatGroupBehavior carried out an action.
+        """
+        ...
+
+    def duplicate_or_similar_order(
+        self,
+        unit: Unit,
+        target: Union[Point2, Unit],
+        order_type: AbilityId,
+        distance_check: float = 0.75,
+    ) -> bool:
+        if cy_distance_to(unit.position, target.position) < distance_check:
+            return True
+
+        if order_target := unit.order_target:
+            # definitely not the same
+            if unit.orders[0].ability.id != order_type:
+                return False
+
+            # the pos we calculated is not that different to previous target
+            if (
+                isinstance(order_target, Point2)
+                and order_target.rounded == target.rounded
+            ):
+                return True
+            if isinstance(order_target, Unit) and order_target == target:
+                return True
+
+        return False
+
+    def group_weapons_on_cooldown(
+        self, group: list[Unit], stutter_forward: bool
+    ) -> bool:
+        avg_weapon_cooldown: float = sum([u.weapon_cooldown for u in group]) / len(
+            group
+        )
+        # all weapons are ready, should stay on attack command
+        if avg_weapon_cooldown <= 0.0:
+            return False
+
+        if stutter_forward:
+            return avg_weapon_cooldown > 2.5
+        else:
+            return avg_weapon_cooldown > 3.5
---
+++ src/ares/behaviors/combat/group/keep_group_safe.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Union
+
+import numpy as np
+from sc2.unit import Unit
+from sc2.units import Units
+
+from ares.behaviors.combat.group import CombatGroupBehavior
+from ares.behaviors.combat.individual import KeepUnitSafe, ShootTargetInRange
+from ares.managers.manager_mediator import ManagerMediator
+
+if TYPE_CHECKING:
+    from ares import AresBot
+
+
+@dataclass
+class KeepGroupSafe(CombatGroupBehavior):
+    """A-Move group to a target.
+
+    Example:
+    ```py
+    from ares.behaviors.combat.group import AMoveGroup
+
+    self.register_behavior(AMoveGroup(units, self.game_info.map_center))
+    ```
+
+    Attributes
+    ----------
+    group : list[Unit]
+        Units we want to control.
+    close_enemy : Union[Units, list[Unit]]
+        Nearby enemy.
+    grid : np.ndarray
+        Grid we should check for safety.
+    attack_in_range_enemy : bool (default=True)
+        Attack in range if weapon is ready.
+    """
+
+    group: list[Unit]
+    close_enemy: Union[Units, list[Unit]]
+    grid: np.ndarray
+    attack_in_range_enemy: bool = True
+
+    def execute(self, ai: "AresBot", config: dict, mediator: ManagerMediator) -> bool:
+        for u in self.group:
+            if self.attack_in_range_enemy:
+                if ShootTargetInRange(u, self.close_enemy).execute(
+                    ai, config, mediator
+                ):
+                    continue
+            KeepUnitSafe(u, self.grid).execute(ai, config, mediator)
+
+        return True
