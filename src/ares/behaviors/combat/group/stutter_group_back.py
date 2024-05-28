0e84b4178fc3ac4cadce87f1ea1e492ec8ac7854
Tom Kerr <tomkerrdev@gmail.com>
2024-05-28 13:02:18 +0100

    feat: improve stutter group back behavior

--- src/ares/behaviors/combat/group/stutter_group_back.py
+++ src/ares/behaviors/combat/group/stutter_group_back.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING, Union
 
 import numpy as np
-from cython_extensions import cy_sorted_by_distance_to
+from cython_extensions import cy_sorted_by_distance_to, cy_towards
 from sc2.ids.ability_id import AbilityId
 from sc2.position import Point2
 from sc2.unit import Unit
@@ -49,17 +49,37 @@ class StutterGroupBack(CombatGroupBehavior):
         sample_unit: Unit = sorted_units[0]
 
         if self.group_weapons_on_cooldown(self.group, stutter_forward=False):
-            group_move_to: Point2 = mediator.find_path_next_point(
-                start=self.group_position,
-                target=ai.start_location,
-                grid=self.grid,
-                sensitivity=9,
-            )
-            if self.duplicate_or_similar_order(
-                sample_unit, group_move_to, AbilityId.MOVE
-            ):
+
+            group_safe: bool = True
+            for unit in self.group:
+                if not mediator.is_position_safe(
+                    grid=self.grid, position=unit.position
+                ):
+                    group_safe = False
+                    break
+            if group_safe:
                 return True
-            ai.give_same_action(AbilityId.MOVE, self.group_tags, group_move_to)
+
+            move_to_target: Point2 = Point2(
+                cy_towards(
+                    self.group_position, self.target.position, -len(self.group) * 1.5
+                )
+            )
+            safe_spot: Point2 = mediator.find_closest_safe_spot(
+                from_pos=move_to_target, grid=self.grid
+            )
+            if ai.in_pathing_grid(safe_spot):
+                group_move_to: Point2 = mediator.find_path_next_point(
+                    start=self.group_position,
+                    target=safe_spot,
+                    grid=self.grid,
+                    sensitivity=min(len(self.group), 8),
+                )
+                if self.duplicate_or_similar_order(
+                    sample_unit, group_move_to, AbilityId.MOVE
+                ):
+                    return True
+                ai.give_same_action(AbilityId.MOVE, self.group_tags, group_move_to)
         else:
             if self.duplicate_or_similar_order(
                 sample_unit, self.target, AbilityId.ATTACK
