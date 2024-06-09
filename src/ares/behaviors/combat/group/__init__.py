145446cc8b0cd2f61ae3a65e2b0e29cb108f6c78
Tom Kerr <tomkerrdev@gmail.com>
2024-06-09 19:55:47 +0100

    fix: add missing import in init file

--- src/ares/behaviors/combat/group/__init__.py
+++ src/ares/behaviors/combat/group/__init__.py
@@ -1,5 +1,6 @@
 from ares.behaviors.combat.group.a_move_group import AMoveGroup
 from ares.behaviors.combat.group.combat_group_behavior import CombatGroupBehavior
+from ares.behaviors.combat.group.group_use_ability import GroupUseAbility
 from ares.behaviors.combat.group.keep_group_safe import KeepGroupSafe
 from ares.behaviors.combat.group.path_group_to_target import PathGroupToTarget
 from ares.behaviors.combat.group.stutter_group_back import StutterGroupBack
