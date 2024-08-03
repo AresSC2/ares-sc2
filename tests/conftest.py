69ea7e491fe504b16cb05009ea36ce29df89741b
Tom Kerr <tomkerrdev@gmail.com>
2024-08-03 19:43:26 +0100

    test: check if fix triggers github action test workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,17 @@
 import asyncio
 import sys
-from os.path import abspath, dirname
+from os.path import abspath, dirname, join
 
 import pytest_asyncio
 
 d = dirname(dirname(abspath(__file__)))
 sys.path.append(f"{d}\\")
 sys.path.append(f"{d}\\src")
+
+d = dirname(dirname(abspath(__file__)))
+sys.path.append(join(d))
+sys.path.append(join(d, 'src'))
+
 
 from tests.load_bot_from_pickle import build_bot_object_from_pickle_data
 
