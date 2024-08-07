6184a8326f160ac09bbb73545e7a18a4ea25fcf8
Tom Kerr <tomkerrdev@gmail.com>
2024-08-07 15:53:00 +0100

    test: add logic to test bots

--- tests/test_bots/micro_bot.py
+++ tests/test_bots/micro_bot.py
@@ -1,9 +1,13 @@
 import random
 import sys
-from os.path import dirname, abspath, join
+from os.path import abspath, dirname, join
 
+import numpy as np
 from sc2 import maps
 from sc2.main import run_game
+from sc2.position import Point2
+
+from ares.behaviors.combat.individual import AMove, KeepUnitSafe, PathUnitToTarget
 
 # Get the directory of the current file
 current_dir = dirname(__file__)
@@ -17,10 +21,11 @@ src_path = join(two_up, "src")
 # Add the src folder to the system path
 sys.path.append(src_path)
 
-from ares import AresBot
-
 from sc2.data import Difficulty, Race
 from sc2.player import Bot, Computer
+
+from ares import AresBot
+from ares.behaviors.combat import CombatManeuver
 
 
 class DummyBot(AresBot):
@@ -29,8 +34,24 @@ class DummyBot(AresBot):
 
     async def on_step(self, iteration: int):
         await super(DummyBot, self).on_step(iteration)
+        # test out a few behaviors while we are here
+        grid: np.ndarray = self.mediator.get_ground_grid
+        target: Point2 = self.enemy_start_locations[0]
+        if self.enemy_structures:
+            target = self.enemy_structures[0].position
+
         for unit in self.units:
-            unit.attack(self.enemy_start_locations[0])
+            maneuver: CombatManeuver = CombatManeuver()
+            if self.time < 10.0:
+                maneuver.add(KeepUnitSafe(unit=unit, grid=grid))
+            else:
+                maneuver.add(
+                    PathUnitToTarget(
+                        unit=unit, grid=grid, target=target, success_at_distance=10.0
+                    )
+                )
+                maneuver.add(AMove(unit=unit, target=target))
+            self.register_behavior(maneuver)
 
     async def on_start(self) -> None:
         await super(DummyBot, self).on_start()
