c8ff8249f1aa6562c293d3ccb99a1ee12a665ff4
tom <tomkerrdev@gmail.com>
2025-01-14 12:47:49 +0000

    feat: add use AOE ability behavior

--- src/ares/behaviors/combat/individual/__init__.py
+++ src/ares/behaviors/combat/individual/__init__.py
@@ -4,12 +4,17 @@ from ares.behaviors.combat.individual.combat_individual_behavior import (
     CombatIndividualBehavior,
 )
 from ares.behaviors.combat.individual.drop_cargo import DropCargo
+from ares.behaviors.combat.individual.ghost_snipe import GhostSnipe
 from ares.behaviors.combat.individual.keep_unit_safe import KeepUnitSafe
+from ares.behaviors.combat.individual.medivac_heal import MedivacHeal
 from ares.behaviors.combat.individual.path_unit_to_target import PathUnitToTarget
 from ares.behaviors.combat.individual.pick_up_cargo import PickUpCargo
+from ares.behaviors.combat.individual.raven_auto_turret import RavenAutoTurret
 from ares.behaviors.combat.individual.shoot_target_in_range import ShootTargetInRange
-from ares.behaviors.combat.individual.speed_mining import SpeedMining
+from ares.behaviors.combat.individual.siege_tank_decision import SiegeTankDecision
 from ares.behaviors.combat.individual.stutter_unit_back import StutterUnitBack
 from ares.behaviors.combat.individual.stutter_unit_forward import StutterUnitForward
 from ares.behaviors.combat.individual.use_ability import UseAbility
+from ares.behaviors.combat.individual.use_aoe_ability import UseAOEAbility
+from ares.behaviors.combat.individual.use_transfuse import UseTransfuse
 from ares.behaviors.combat.individual.worker_kite_back import WorkerKiteBack
---
+++ src/ares/dicts/aoe_ability_to_range.py
@@ -0,0 +1,43 @@
+from sc2.ids.ability_id import AbilityId
+from sc2.ids.buff_id import BuffId
+from sc2.ids.effect_id import EffectId
+
+AOE_ABILITY_SPELLS_INFO: dict[AbilityId, dict] = {
+    AbilityId.BLINDINGCLOUD_BLINDINGCLOUD: {
+        "radius": 2.0,
+        "range": 10.0,
+        "effect": EffectId.BLINDINGCLOUDCP,
+    },
+    AbilityId.EFFECT_CORROSIVEBILE: {
+        "radius": 0.5,
+        "range": 9.0,
+        "effect": EffectId.RAVAGERCORROSIVEBILECP,
+    },
+    AbilityId.EFFECT_ANTIARMORMISSILE: {
+        "radius": 2.88,
+        "range": 10.0,
+        "effect": BuffId.RAVENSHREDDERMISSILEARMORREDUCTION,
+    },
+    AbilityId.EMP_EMP: {"radius": 1.5, "range": 10.0, "effect": None},
+    AbilityId.FUNGALGROWTH_FUNGALGROWTH: {
+        "radius": 2.25,
+        "range": 10.0,
+        "effect": BuffId.FUNGALGROWTH,
+    },
+    AbilityId.KD8CHARGE_KD8CHARGE: {"radius": 2.0, "range": 5.0, "effect": None},
+    AbilityId.PARASITICBOMB_PARASITICBOMB: {
+        "radius": 3.0,
+        "range": 8.0,
+        "effect": BuffId.PARASITICBOMB,
+    },
+    AbilityId.PSISTORM_PSISTORM: {
+        "radius": 1.5,
+        "range": 9.0,
+        "effect": EffectId.PSISTORMPERSISTENT,
+    },
+    AbilityId.TACNUKESTRIKE_NUKECALLDOWN: {
+        "radius": 12.0,
+        "range": 9.0,
+        "effect": EffectId.NUKEPERSISTENT,
+    },
+}
