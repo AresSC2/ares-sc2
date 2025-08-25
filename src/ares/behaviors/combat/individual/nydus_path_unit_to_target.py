20b20ca86cf20912545e436a119ac3948769b9ba
rasper <tomkerrdev@gmail.com>
2025-08-25 21:37:54 +0100

    feat: implement NydusPathUnitToTarget behavior
    - Introduce `NydusPathUnitToTarget` class for pathing units to targets using Nydus worms.
    - Add behavior attributes such as grid, sensitivity, and smoothing.
    - Support both Nydus traversal and fallback to regular pathing.

---
+++ src/ares/behaviors/combat/individual/nydus_path_unit_to_target.py
@@ -0,0 +1,83 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+import numpy as np
+from cython_extensions import cy_distance_to
+from sc2.position import Point2
+from sc2.unit import Unit
+
+from ares.behaviors.combat.individual.combat_individual_behavior import (
+    CombatIndividualBehavior,
+)
+from ares.managers.manager_mediator import ManagerMediator
+
+if TYPE_CHECKING:
+    from ares import AresBot
+
+
+@dataclass
+class NydusPathUnitToTarget(CombatIndividualBehavior):
+    """Path a unit to its target destination, using nyduses if
+    available.
+
+    Example:
+    ```py
+    from ares.behaviors.combat.individual import NydusPathToTarget
+
+    unit: Unit
+    grid: np.ndarray = self.mediator.get_ground_grid
+    target: Point2 = self.game_info.map_center
+    self.register_behavior(NydusPathToTarget(unit, grid, target))
+    ```
+
+    Attributes:
+        unit: The unit to path.
+        grid: 2D grid to path on.
+        target: Target destination.
+        success_at_distance: float = 0.0
+        large: Path large units. Defaults to False.
+        sensitivity: Path precision. Defaults to 5.
+        smoothing: Whether to smooth out the path. Defaults to False.
+    """
+
+    unit: Unit
+    grid: np.ndarray
+    target: Point2
+    success_at_distance: float = 0.0
+    large: bool = (False,)
+    sensitivity: int = 5
+    smoothing: bool = False
+
+    def execute(self, ai: "AresBot", config: dict, mediator: ManagerMediator) -> bool:
+        distance_to_target: float = cy_distance_to(self.unit.position, self.target)
+        # no action executed
+        if distance_to_target < self.success_at_distance:
+            return False
+
+        point, exit_towards, nydus_tags = mediator.find_nydus_path_next_point(
+            start=self.unit.position,
+            target=self.target,
+            grid=self.grid,
+            sensitivity=self.sensitivity,
+            smoothing=self.smoothing,
+        )
+        if nydus_tags and self.unit.tag not in mediator.get_banned_nydus_travellers:
+            mediator.add_to_nydus_travellers(
+                unit=self.unit,
+                entry_nydus_tag=nydus_tags[0],
+                exit_nydus_tag=nydus_tags[1],
+                exit_towards=exit_towards,
+            )
+
+            if Point2(point) == ai.unit_tag_dict[nydus_tags[0]].position.rounded:
+                self.unit.smart(ai.unit_tag_dict[nydus_tags[0]])
+            else:
+                self.unit.move(point)
+        # No nydus tags, revert to normal pathing
+        else:
+            if point:
+                self.unit.move(point)
+            else:
+                self.unit.move(self.target)
+
+        return True
